c4682176989a4a107a5b927e477e7b8a82071649
theerayutbee2526-cloud <theerayutbee2526@gmail.com>
2026-09-07 11:34:51 +0700

    Update app-config.js

--- app-config.js
+++ app-config.js
@@ -2,6 +2,6 @@
   'use strict';
 
   window.APP_CONFIG = Object.freeze({
-    API_URL: 'https://script.google.com/macros/s/AKfycbw7gUWih1UGW37lxpi1jj98G0k35iVN7FVRMP-E4HeMs0ea54oTtAI9f2uj4OYHPx4w/exec'
+    API_URL: 'https://script.google.com/macros/s/AKfycbx0uWcrUYFvifBeG1P-saoCTR2UFlFxcTrVYelvwB_vHxiEFTqdl12Ky2hQ3_7O4oYl/exec'
   });
 })();
