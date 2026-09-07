6ebcc54ee1932d0354a8789ca233c1f26d31cc52
theerayutbee2526-cloud <theerayutbee2526@gmail.com>
2026-09-07 11:36:19 +0700

    Update profile-config.js

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbyWmfWXRdB2d2S10fV7B4BXNQjoaIVmHRYfGwSyIEU79i6BzFnwXWdpqJhq7WLohHBaZw/exec';
